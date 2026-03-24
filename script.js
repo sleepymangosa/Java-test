3d90b3b24bba114895348892f84cfde49b0adca2
Royston <roystonclementson15@gmail.com>
2026-03-24 13:18:57 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+//Variables
+let age = 18;
+
+//Functions
+function changeText(){
+    document.getElementById("demo").innerHTML = "Welcome"
+}
+
+//DOM Manipulation
+let name = "Roy"
+
+function showName(){
+    document.getElementById("output").textContent = name;
+}
+
+showName();
